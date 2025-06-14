a64a8136b6d8e323059313564209da394bdd6ee8
Crowley55555 <lavrovartem0511@gmail.com>
2025-06-14 13:01:13 +0300

    функция добавления новостей и удаления их из админки рализована

--- cyberblog/news/admin.py
+++ cyberblog/news/admin.py
@@ -3,8 +3,30 @@ from .models import News
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'source', 'created_at', 'is_published')
-    list_filter = ('is_published', 'created_at', 'source')
-    search_fields = ('title', 'content', 'source')
+    list_display = ('title', 'author_name', 'source', 'created_at', 'is_published', 'moderation_status')
+    list_filter = ('is_published', 'moderation_status', 'created_at')
+    search_fields = ('title', 'content', 'author_name', 'source')
     prepopulated_fields = {'slug': ('title',)}
     date_hierarchy = 'created_at'
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('title', 'slug', 'content', 'image', 'source', 'author_name')
+        }),
+        ('Статус', {
+            'fields': ('is_published', 'moderation_status', 'moderation_comment')
+        }),
+        ('Даты', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+    actions = ['make_published', 'make_unpublished']
+
+    def make_published(self, request, queryset):
+        queryset.update(is_published=True, moderation_status='approved')
+    make_published.short_description = "Опубликовать выбранные новости"
+
+    def make_unpublished(self, request, queryset):
+        queryset.update(is_published=False)
+    make_unpublished.short_description = "Снять с публикации выбранные новости"
